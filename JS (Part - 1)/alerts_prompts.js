be87cc1d963421514d6384812fbf703a63423f97
Priyanshu Raj <er.priyansu@gmail.com>
2026-01-14 22:41:09 +0530

    initial commit

---
+++ JS (Part - 1)/alerts_prompts.js
@@ -0,0 +1,13 @@
+alert("something went worng.");
+console.log("This is a simple log");
+console.error("This is an error msg.");
+console.warn("This is a warning msg.");
+
+let firstName = prompt("Enter first name:");
+let lastName = prompt("Enter last name:");
+
+let msg = "Welcome " + firstName + " " + lastName + "!";
+alert(msg);
+
+let rollno = prompt("Enter Roll No:");
+console.log(rollno);
