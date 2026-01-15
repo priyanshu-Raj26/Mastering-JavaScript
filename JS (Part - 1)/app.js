79ef7446a6dc216c72d93192c80456e8298130ff
Priyanshu Raj <er.priyansu@gmail.com>
2026-01-15 18:25:36 +0530

    initial commit

---
+++ JS (Part - 1)/app.js
@@ -0,0 +1,16 @@
+console.log("Hello world");
+console.log("Hello JavaScript");
+
+// let a = 5;
+// let b = 10;
+// console.log("Sum is : ", a+b);
+//this is a comment
+
+let pencilePrice = 10;
+let eraserPrice = 5;
+
+// let output = "The total price is : "+ (pencilePrice+eraserPrice)+ " Rupees."
+// console.log("The total price is :", pencilePrice+eraserPrice, "Rupees.");
+
+let output = `The total price is : ${pencilePrice+eraserPrice} Rupees.`  //use of literal.
+console.log(output);
