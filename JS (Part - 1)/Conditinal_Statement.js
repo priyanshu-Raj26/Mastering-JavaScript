d24a55bf79eb70810d11658ce91280fcc7b4f9c3
Priyanshu Raj <er.priyansu@gmail.com>
2026-01-17 00:35:10 +0530

    initial commit

---
+++ JS (Part - 1)/Conditinal_Statement.js
@@ -0,0 +1,19 @@
+// Conditional Statement:-
+console.log("Before if statement");
+let age = 23;
+
+// if (age >= 18) {
+//     console.log("You can vote.")
+//     console.log("You can drive");
+// }
+
+if (age >=20) {
+    console.log("You are in your 20s.");
+}
+
+let firstName = "Priyanshu";
+if (firstName == "Priyanshu") {
+    console.log(`Welcome ${firstName}`);
+}
+
+console.log("After if statement.");
