6d84dd880eade50faef7901dd92e7f652ace871e
Priyanshu Raj <er.priyansu@gmail.com>
2026-01-20 15:46:16 +0530

    initial commit

---
+++ JS (Part - 1)/Largest_of_Three.js
@@ -0,0 +1,11 @@
+let num1 = 69;
+let num2 = 29;
+let num3 = 96;
+
+if (num1 > num2 && num1 > num3) {
+    console.log(`${num1} is grerater`);
+} else if (num2 > num3) {
+    console.log(`${num2} is greater`);
+} else {
+    console.log(`${num3} is greater`);
+}
