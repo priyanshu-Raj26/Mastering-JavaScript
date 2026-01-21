499b4c90c218ea8706c5dd6a30faa71351e14bf7
Priyanshu Raj <er.priyansu@gmail.com>
2026-01-21 15:31:08 +0530

    initial commit

---
+++ JS (Part - 1)/Operators.js
@@ -0,0 +1,29 @@
+let a = 10;
+let b = 5;
+
+// Arithmetic Operators:-
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+// Unary Operators:-
+console.log(a++); //10
+console.log(++a); //12
+
+// Assignement Operators:-
+let c = b;
+console.log(c);
+
+// Comparision Operators:-
+let age = 18;
+console.log(age < 23);
+
+let n = 5;
+let str = '5';
+
+console.log(5 != 5);
+console.log(n == str); // compare value not type
+console.log(n === str); // compare value and type
