d00ef8c2ee62d56f1a599ce4fd0caedfbdedd5dd
Priyanshu Raj <er.priyansu@gmail.com>
2026-01-22 22:02:34 +0530

    initial commit

---
+++ JS (Part - 1)/PracticeQ1.js
@@ -0,0 +1,23 @@
+let color = "green";
+
+// Traffic light system.
+
+if (color === "red") {
+    console.log("STOP!, light color is red");
+}else if (color === "yellow, light color is yellow") {
+    console.log("SLOW DOWN");
+}else if (color === "green") {
+    console.log("GO, light color is green");
+}
+
+
+
+let month = "january";
+
+if (month === "january") {
+     console.log("Winter is here.");
+} else if (month === "april") {
+    console.log("Summer is here.");
+} else if (month === "August") {
+    console.log("Rainney is here.");
+}
