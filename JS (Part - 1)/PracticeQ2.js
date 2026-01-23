b9c74214aaa0b627f0db842145e1b03d4c165de5
Priyanshu Raj <er.priyansu@gmail.com>
2026-01-23 23:36:17 +0530

    initial commit

---
+++ JS (Part - 1)/PracticeQ2.js
@@ -0,0 +1,24 @@
+// let size = "L";
+
+// if (size === "XL") {
+//     console.log("Price is RS. 250");
+// } else if (size === "L") {
+//     console.log("Price is RS. 200");
+// }else if (size === "M") {
+//     console.log("Price is RS. 100");
+// }else {
+//     console.log("Price is RS. 50");
+// }
+
+let marks = 45;
+
+if (marks >= 33) {
+    console.log("PASS");
+    if (marks >= 80) {
+        console.log("Grade : O");
+    }else {
+        console.log("Grade : A");
+    }
+} else {
+    console.log("Better luck next time.");
+}
