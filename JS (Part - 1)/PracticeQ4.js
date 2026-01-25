3dde6e06d18de2664271248076e1bc13d8156534
Priyanshu Raj <er.priyansu@gmail.com>
2026-01-25 19:30:11 +0530

    initial commit

---
+++ JS (Part - 1)/PracticeQ4.js
@@ -0,0 +1,10 @@
+// Create a number variable num with some value,
+    //Now, print "good" if number is divisible by 10 and print "bad" if it's not.
+
+let num = 40;
+
+if (num % 10 === 0) {
+    console.log("good");
+} else {
+    console.log("bad");
+}
