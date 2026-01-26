6c09a7fc7c8798f30903233589b532227e0bcb5c
Priyanshu Raj <er.priyansu@gmail.com>
2026-01-26 23:33:44 +0530

    initial commit

---
+++ JS (Part - 1)/PracticeQ7.js
@@ -0,0 +1,8 @@
+// let str = "Enginner";
+let str = "Apples";
+
+if ((str[0] === 'a' || str[0] === "A") && str.length > 5) {
+    console.log("Good String");
+} else {
+    console.log("Not a good String");
+}
