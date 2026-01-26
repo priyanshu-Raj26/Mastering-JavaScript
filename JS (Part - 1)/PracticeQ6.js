7b6730e257ad4c14c9be436483aa18da7066de1b
Priyanshu Raj <er.priyansu@gmail.com>
2026-01-26 23:32:04 +0530

    initial commit

---
+++ JS (Part - 1)/PracticeQ6.js
@@ -0,0 +1,14 @@
+let Quater = 4;
+
+switch(Quater) {
+    case 1:
+        console.log("January, Febrary, March");
+    case 2:
+        console.log("April, May, June");
+    case 3:
+        console.log("July, August, September");
+    case 4:
+        console.log("October, November, December");
+    default:
+        console.log("Wrong Input");
+}
