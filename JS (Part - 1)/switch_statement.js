7b17dbda49a9851721634e378cdce4877fd615d9
Priyanshu Raj <er.priyansu@gmail.com>
2026-01-27 23:25:26 +0530

    initial commit

---
+++ JS (Part - 1)/switch_statement.js
@@ -0,0 +1,43 @@
+// let color = "green";
+
+// switch(color) {
+//     case "red":
+//         console.log("stop");
+//         break;
+//     case "yellow":
+//         console.log("slow down");
+//         break;
+//     case "green":
+//         console.log("Go");
+//         break;
+//     default:
+//         console.log("Light is Broken");
+// }
+
+let day = 3;
+
+switch(day) {
+    case 1:
+        console.log("It's Monday");
+        break;
+    case 2:
+        console.log("It's Tuesday");
+        break;
+    case 3:
+        console.log("It's Wednesday");
+        break;
+    case 4:
+        console.log("It's Thuresday");
+        break;
+    case 5:
+        console.log("It's Friday");
+        break;
+    case 6:
+        console.log("It's Saturday");
+        break;
+    case 7:
+        console.log("It's Sunday");
+        break;
+    default:
+        console.log("Worng Input!");
+}
