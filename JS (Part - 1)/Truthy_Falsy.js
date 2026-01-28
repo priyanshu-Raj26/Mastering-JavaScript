0c97937f28e61f22e713ada4099ef0d938d515ee
Priyanshu Raj <er.priyansu@gmail.com>
2026-01-28 22:51:20 +0530

    initial commit

---
+++ JS (Part - 1)/Truthy_Falsy.js
@@ -0,0 +1,23 @@
+let string = " ";
+
+if (string) {
+    console.log("String is not empty.");
+} else {
+    console.log("String is empty.");
+}
+
+let num = 0;
+
+if (num) {
+    console.log("num is not equal to zero");
+} else {
+    console.log("num is equal to zero.");
+}
+
+let name = "Priyanshu";
+
+if(name) {
+    console.log("It has true value.");
+} else {
+    console.log("It has false value.");
+}
