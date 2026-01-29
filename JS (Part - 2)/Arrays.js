d9e87e300d4a4b0a2cc6017ce056efd31a1aaa58
Priyanshu Raj <er.priyansu@gmail.com>
2026-01-29 20:28:48 +0530

    initial commit

---
+++ JS (Part - 2)/Arrays.js
@@ -0,0 +1,31 @@
+let Students = ["Aman", "Ishu", "Rajat"];  //array
+
+let cars = ["audi", "bmw", "xuv", "maruti"];
+
+console.log(cars.slice(-2));
+
+console.log(cars.splice(3));
+
+console.log(cars.splice(0,1));
+
+console.log(cars.splice(0,1));
+cars.push("maruti");
+cars.push("ferrari");
+
+console.log(cars);
+
+console.log(cars.splice(1,2));
+
+console.log(cars.splice(0,0,"toyota", "xuv", "maruti"));
+
+console.log(cars);
+
+console.log(cars.splice(1,0,"mercedes"));
+
+console.log(cars);
+
+console.log(cars.splice(3,3,"G-Wagon"));
+
+let arr = ['a', 'b', 'c'];
+
+let arrCopy = arr;
