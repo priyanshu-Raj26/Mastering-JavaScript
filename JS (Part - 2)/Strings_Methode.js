ed3c4187d376ea3d8e087c6bbb511e83655996d2
Priyanshu Raj <er.priyansu@gmail.com>
2026-01-30 19:28:54 +0530

    initial commit

---
+++ JS (Part - 2)/Strings_Methode.js
@@ -0,0 +1,36 @@
+// let str = "           Hello          ";
+
+// let password = prompt("set your password");
+// let newpass = password.trim();
+
+// console.log(newpass);
+
+let name = "Apna College";
+let str = "error";
+
+console.log(name.toLowerCase());
+console.log(str.toUpperCase());
+
+let msg = "ILoveCoding";
+
+let text = "    hello";
+
+newMsg = text.trim().toUpperCase();
+console.log(newMsg);
+
+// slice Methode:-
+let info = "apnacollege";
+console.log(info.slice(4));
+console.log(info.slice(-4));  //info.length-4  (11-4)
+
+// replace Methode:-
+let str2 = "ILoveCoding";
+
+console.log(str2.replace("Love", "do"));
+let str3 = "apna college apna";
+console.log(str3.replace("apna", "college"));
+
+// repeat Methode:-
+let string = "Priyanshu";
+
+console.log(string.repeat(3));
