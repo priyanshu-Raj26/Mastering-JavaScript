987c1115938045dda1f37fddc28b50c27b380b64
Priyanshu Raj <er.priyansu@gmail.com>
2026-02-01 19:47:38 +0530

    initial commit

---
+++ JS (Part - 2)/PracticeQ1.js
@@ -0,0 +1,8 @@
+// Qs1. Write a JavaScript program to get the first n elements of an array. [n can be any  positive number].
+
+let arr = [7, 9, 0, -2];
+let n = 3;
+
+let ans = arr.slice(0, n);
+
+console.log(ans);
