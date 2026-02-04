6f0e61e5110748c5f56aa8fa5a292aeafda2c639
Priyanshu Raj <er.priyansu@gmail.com>
2026-02-04 20:12:20 +0530

    initial commit

---
+++ JS (Part - 2)/PracticeQ4.js
@@ -0,0 +1,11 @@
+// Qs4. Write a JavaScript program to test whether the character at the given (character) index is lower case.
+
+let str = "ILovECrIcKeT";
+let idx = 2;
+
+if (str[idx] == str[idx].toLowerCase()) {
+    console.log("Character is lowercase");
+} else {
+    console.log("Character is not a lowercase")
+}
+
