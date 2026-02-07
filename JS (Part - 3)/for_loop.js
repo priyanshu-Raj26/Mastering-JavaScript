252e3d9d24b8cfde32b8f37bccee45e43ee9eee6
Priyanshu Raj <er.priyansu@gmail.com>
2026-02-07 19:49:07 +0530

    initial commit

---
+++ JS (Part - 3)/for_loop.js
@@ -0,0 +1,17 @@
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+
+for (let i = 10; i >= 1; i-=3) {
+    console.log(i);
+}
+
+console.log(i);  // ReferenceError: i is not defined
+
