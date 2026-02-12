84d2849622ed077ad9b309046a2e9e9d91b0413e
Priyanshu Raj <er.priyansu@gmail.com>
2026-02-12 19:34:58 +0530

    initial commit

---
+++ JS (Part - 3)/Print_Odd_And_Even_NO.js
@@ -0,0 +1,12 @@
+console.log("Print all odd no between 1 to 15");
+
+for (let i = 1; i <= 15; i+=2) {
+    console.log(i);
+}
+
+
+console.log("Print all even no between 1 to 15");
+
+for (let i = 2; i <= 15; i+=2) {
+    console.log(i);
+}
