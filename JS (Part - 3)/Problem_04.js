11fc05748ef47faae8054fab0e02b7ef6a3cdd40
Priyanshu Raj <er.priyansu@gmail.com>
2026-02-16 19:11:03 +0530

    initial commit

---
+++ JS (Part - 3)/Problem_04.js
@@ -0,0 +1,10 @@
+// Qs4. Print the factorial of a number n
+
+let n = 5;
+let result = 1;
+
+for (let i = 1; i <= n; i++) {
+    result *= i;
+}
+
+console.log(`Factorial of a number ${n} is: ${result}`);
