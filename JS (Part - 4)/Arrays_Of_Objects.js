6b925b79d56bd249d012d9a7b0717bbf1d042d49
Priyanshu Raj <er.priyansu@gmail.com>
2026-02-21 19:37:47 +0530

    initial commit

---
+++ JS (Part - 4)/Arrays_Of_Objects.js
@@ -0,0 +1,28 @@
+const classInfo = [
+    {
+        name: "priyanshu",
+        grade: "A+",
+        city: "patna"
+    },
+    {
+       name: "aman",
+       grade: "A",
+       city: "delhi"
+    },
+    {
+        name: "shradha",
+        grade: "O",
+        city: "gurgaon"
+    },
+]
+
+console.log(classInfo[0]);
+
+console.log(classInfo[1].name);
+
+console.log(classInfo[1].city);
+
+classInfo[2].genter = "female";
+
+
+console.log(classInfo[2]);;
