212c551bdc2975a66ef428b0ed0a7b87be9d516c
Priyanshu Raj <er.priyansu@gmail.com>
2026-02-23 21:24:56 +0530

    initial commit

---
+++ JS (Part - 4)/Get_Value.js
@@ -0,0 +1,14 @@
+const obj = {
+    1: "a", 
+    2: "b",
+    true: "c", 
+    null: "d", 
+    undefined: "e"
+};
+
+//converts object to string
+console.log(obj[1]); 
+console.log(obj[2]);
+console.log(obj[true]);
+console.log(obj[null]);
+console.log(obj[undefined]);
