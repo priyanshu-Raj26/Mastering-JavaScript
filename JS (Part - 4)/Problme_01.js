2bc7ced226421e3112180aeee985a1716c2678c1
Priyanshu Raj <er.priyansu@gmail.com>
2026-02-26 23:15:32 +0530

    initial commit

---
+++ JS (Part - 4)/Problme_01.js
@@ -0,0 +1,10 @@
+// Qs2. Create an object representing a car that stores the following properties for the car: name, model, color.
+// Print the car’s name.
+
+const car = {
+    name: "Bmw",
+    Model: "M4_Comp",
+    color: "black"
+};
+
+console.log(car.name);
