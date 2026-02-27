f279e810cd7c266388718bd7f0e2c24ebff31837
Priyanshu Raj <er.priyansu@gmail.com>
2026-02-27 21:11:45 +0530

    initial commit

---
+++ JS (Part - 4)/Problme_02.js
@@ -0,0 +1,13 @@
+// Qs3. Create an object Person with their name, age and city. Edit their city’s original value to change it to “New York”.
+// Add a new property country and set it to the United States.
+
+const Person = {
+    name: "Priyanshu Raj",
+    age: 20,
+    city: "Patna"
+};
+
+Person.city = "New York";
+Person.country = "United States";
+
+console.log(Person);
