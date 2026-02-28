280a994ee6b3285285dcba35f2f889db2e7e3a35
Priyanshu Raj <er.priyansu@gmail.com>
2026-02-28 23:06:17 +0530

    initial commit

---
+++ JS (Part - 4)/Update_Value.js
@@ -0,0 +1,14 @@
+let student = {
+    name: "Priyanshu Raj",
+    age: 20,
+    marks: 85.5,
+    city: "delhi"
+};
+
+
+student.city = "mumbai";  //change value
+student.marks = [85.5, 94, 75];  //change value to array
+
+student.gender = "male";  //add value
+
+delete student.gender;   //delete value
