f9dc9128f7bd41d5bfa3a557d4a3d0f530c8ab4b
Priyanshu Raj <er.priyansu@gmail.com>
2026-03-02 19:44:58 +0530

    initial commit

---
+++ JS (Part - 5)/Function_Expression.js
@@ -0,0 +1,15 @@
+let name = "Priyanshu";
+
+let sum = function(a, b) {
+    return a+b;
+}
+
+console.log(sum(1,2));
+
+let hello = function() {
+    console.log("hello");
+}
+
+hello = function() {
+    console.log("nameste");
+}
