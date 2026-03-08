e6f99641511991b86531f8ec610f4bd601a6b792
Priyanshu Raj <er.priyansu@gmail.com>
2026-03-08 17:33:48 +0530

    initial commit

---
+++ JS (Part - 5)/Methods.js
@@ -0,0 +1,29 @@
+// const calculator = {
+//     num: 10,
+//     add: function(a, b) {
+//         return a+b;
+//     },
+//     sub: function(a, b) {
+//         return a-b;
+//     },
+//     mul: function(a, b) {
+//         return a*b;
+//     }
+// };
+
+// SHORT_HAND:-
+
+
+const calculator = {
+    num: 10,
+    add(a, b) {
+        return a+b;
+    },
+    sub(a, b) {
+        return a-b;
+    },
+    mul(a, b) {
+        return a*b;
+    }
+};
+
