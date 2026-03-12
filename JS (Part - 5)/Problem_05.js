7c84322fd19062bbff720b3c6ceb445ed2f545eb
Priyanshu Raj <er.priyansu@gmail.com>
2026-03-12 19:46:45 +0530

    initial commit

---
+++ JS (Part - 5)/Problem_05.js
@@ -0,0 +1,18 @@
+// Qs3. Write a JavaScript function that accepts a list of country names as input and returns the longest country name as output.
+// Example : country = ["Australia", "Germany", "United States of America"] output : "United States of America"
+
+let country = ["United States of America", "Australia", "Germany",];
+
+function longestName(country) {
+    let ansIdx = 0;
+    for (let i = 0; i < country.length; i++) {
+        let ansLen = country[ansIdx].length;
+        let currLen = country[i].length;
+        if (currLen > ansLen) {
+            ansIdx = i;
+        }
+    }
+    return country[ansIdx];
+}
+
+console.log(longestName(country));
