d9276fc2db2dc9c89e42e94de5d2d9b141e5d1d3
Priyanshu Raj <er.priyansu@gmail.com>
2026-03-22 19:49:20 +0530

    initial commit

---
+++ JS (Part - 6)/Problem_03.js
@@ -0,0 +1,14 @@
+// write an arrow function named isEven() that takes a single number as argunment and returns if it is even or not.
+
+
+const isEven = (n) => n % 2 == 0;
+
+// const isEven = n => {
+//     if (n % 2 == 0) {
+//         return "Even";
+//     } else {
+//         return "Odd";
+//     }
+// }
+
+console.log(isEven(4));
