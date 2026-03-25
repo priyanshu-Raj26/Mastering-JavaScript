76d49aac4a9cdf10392270a6b3ac427152f129a8
Priyanshu Raj <er.priyansu@gmail.com>
2026-03-25 21:16:26 +0530

    initial commit

---
+++ JS (Part - 6)/SetInterval_func.js
@@ -0,0 +1,18 @@
+let id = setInterval( () => {
+    console.log("apna college");
+}, 2000);
+
+console.log(id);
+
+
+let id2 = setInterval( () => {
+    console.log("Enginnering is End");
+}, 3000);
+
+console.log(id2);
+
+
+//for stoping execution:- 
+
+// clearInterval(id)
+// clearInterval(id2)
