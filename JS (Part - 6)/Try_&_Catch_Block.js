a6495a6c1c32155a860ab10c60e449117b2ac168
Priyanshu Raj <er.priyansu@gmail.com>
2026-03-29 20:50:37 +0530

    initial commit

---
+++ JS (Part - 6)/Try_&_Catch_Block.js
@@ -0,0 +1,14 @@
+console.log("hello");
+console.log("hello");
+console.log("hello");
+// let a = 4;
+try {
+    console.log(a);
+} catch (err) {
+    console.log("caught an error..");
+    console.log(err);
+}
+
+console.log("hello2");
+console.log("hello2");
+console.log("hello2");
