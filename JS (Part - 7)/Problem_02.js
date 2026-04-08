ddc3765d8307ef6033db844a87fba1b126672db0
Priyanshu Raj <er.priyansu@gmail.com>
2026-04-08 20:04:08 +0530

    initial commit

---
+++ JS (Part - 7)/Problem_02.js
@@ -0,0 +1,7 @@
+// Qs2. Create a new array using the map function whose each element is equal to the  original element plus 5.
+
+let arr = [2, 4, 6, 8];
+
+let newArr = arr.map( (el) => el+5);
+
+console.log(newArr);
