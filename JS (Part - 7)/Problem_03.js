b16eef9bfa0a79cc4edcfa158f309cc4d0c8df5f
Priyanshu Raj <er.priyansu@gmail.com>
2026-04-09 20:03:16 +0530

    initial commit

---
+++ JS (Part - 7)/Problem_03.js
@@ -0,0 +1,7 @@
+// Qs3. Create a new array whose elements are in uppercase of words present in the original array.
+
+let arr = ["hii", "hello", "bye", "tata"];
+
+let newArr = arr.map( (char) => char.toUpperCase());
+
+console.log(newArr);
