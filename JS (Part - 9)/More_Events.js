36e86673dd450fa545d5af2d17eee7fa4e810ed0
Priyanshu Raj <er.priyansu@gmail.com>
2026-04-25 18:13:42 +0530

    initial commit

---
+++ JS (Part - 9)/More_Events.js
@@ -0,0 +1,27 @@
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+});
+
+let user = document.querySelector("#user");
+
+// user.addEventListener("change", function () {
+//     console.log("change event");
+//     console.log("final value = ", this.value);
+// })
+
+
+// user.addEventListener("input", function () {
+//     console.log("input event");
+//     console.log("final value = ", this.value);
+// })
+
+//MY test editor:-
+let inp = document.querySelector("#text");
+let p = document.querySelector("p");
+
+inp.addEventListener("input", function () {
+    console.log(inp.value);
+    p.innerText = inp.value;
+})
